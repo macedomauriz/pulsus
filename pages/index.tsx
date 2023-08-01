79b76f47d4896932e1eaed9fe44b8c318ddda877
Rodrigo Macedo <rodrigo1987macedo@gmail.com>
2023-08-01 05:57:52 -0300

    home first approach

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,11 @@
 import * as React from "react"
-import Container from "@mui/material/Container"
-import Typography from "@mui/material/Typography"
-import Box from "@mui/material/Box"
+import { Container } from "@mui/material"
+import Hero from "components/Hero"
 
 export default function Index() {
   return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Typography variant="h4" component="h1" gutterBottom>
-          Material UI - Next.js example in TypeScript
-        </Typography>
-      </Box>
+    <Container maxWidth="xl" disableGutters={true}>
+      <Hero />
     </Container>
   )
 }
