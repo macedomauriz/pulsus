fc17eacfa42da5df5ee2223bcb9b6fcf844a0091
Rodrigo Macedo <rodrigo1987macedo@gmail.com>
2023-08-11 11:18:59 -0300

    404 style

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,16 @@
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  return (
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <h3 style={{ margin: "0 10px 0 0" }}>404</h3>
+      <p style={{ margin: 0 }}>Page not found</p>
+    </main>
+  )
 }
